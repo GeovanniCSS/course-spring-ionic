a52ce949f3fd63ae1fba035f2fc83e8c1290a0e5
Geovanni Corsino <geovannicorsino@gmail.com>
2021-01-05 16:26:50 -0300

    BeanValidation

--- src/main/java/com/course/coursemc/dto/CategoriaDTO.java
+++ src/main/java/com/course/coursemc/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.course.coursemc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.course.coursemc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message = "O nome não pode ficar em branco")
+	@Length(min = 5,max = 80, message = "O nome deve ter entre {min} e {max} caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
--- src/main/java/com/course/coursemc/services/CategoriaService.java
+++ src/main/java/com/course/coursemc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.course.coursemc.domain.Categoria;
+import com.course.coursemc.dto.CategoriaDTO;
 import com.course.coursemc.repositories.CategoriaRepository;
 import com.course.coursemc.services.exceptions.DataIntegrityException;
 import com.course.coursemc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO dto) {
+		return new Categoria(dto.getId(), dto.getNome());
 	}
 }
