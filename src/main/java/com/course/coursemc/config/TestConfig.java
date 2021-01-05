7a80d4edbeb882cae5d1e2250dbfdd8cf6687700
Geovanni Corsino <geovannicorsino@gmail.com>
2021-01-05 15:58:57 -0300

    categoriaPage

--- src/main/java/com/course/coursemc/config/TestConfig.java
+++ src/main/java/com/course/coursemc/config/TestConfig.java
@@ -55,17 +55,23 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
 
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(1, "Informática");
 		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria cat3 = new Categoria(1, "fIFA");
+		Categoria cat4 = new Categoria(2, "sHOW");
+		Categoria cat5 = new Categoria(1, "haha");
+		Categoria cat6 = new Categoria(2, "afsafas");
+		Categoria cat7 = new Categoria(1, "sete");
+		Categoria cat8 = new Categoria(2, "oito");
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -78,7 +84,7 @@ public class TestConfig implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
