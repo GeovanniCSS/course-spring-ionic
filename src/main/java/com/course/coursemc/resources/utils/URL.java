99d64ca71be29f2ac14a79d2a25b597618382555
Geovanni Corsino <geovannicorsino@gmail.com>
2021-01-06 09:37:50 -0300

    Pesquisa de produtos

---
+++ src/main/java/com/course/coursemc/resources/utils/URL.java
@@ -0,0 +1,22 @@
+package com.course.coursemc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class URL {
+
+	public static String decodeParam(String s) {
+		try {
+			return URLDecoder.decode(s, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+
+	public static List<Integer> decodeIntList(String ids) {
+		return Arrays.asList(ids.split(",")).stream().map(id -> Integer.parseInt(id)).collect(Collectors.toList());
+	}
+}
--- src/main/java/com/course/coursemc/services/ProdutoService.java
+++ src/main/java/com/course/coursemc/services/ProdutoService.java
@@ -1,24 +1,40 @@
 package com.course.coursemc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.course.coursemc.domain.Categoria;
 import com.course.coursemc.domain.Produto;
+import com.course.coursemc.repositories.CategoriaRepository;
 import com.course.coursemc.repositories.ProdutoRepository;
 import com.course.coursemc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ProdutoService {
-	
+
 	@Autowired
 	private ProdutoRepository repository;
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public Produto find(Integer id) {
 		Optional<Produto> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
 	}
-	
+
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy,
+			String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repository.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+
 }
