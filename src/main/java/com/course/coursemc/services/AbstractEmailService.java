ed470ed9d229ece4cb0a690ed7db85acb231fd03
Geovanni Corsino <geovannicorsino@gmail.com>
2021-01-07 16:40:10 -0300

    Envio de e-mail no perfil dev

--- src/main/java/com/course/coursemc/services/AbstractEmailService.java
+++ src/main/java/com/course/coursemc/services/AbstractEmailService.java
@@ -4,9 +4,11 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
 
 import com.course.coursemc.domain.Pedido;
 
+@Component
 public abstract class AbstractEmailService implements EmailService {
 
 	@Value("${default.sender}")
