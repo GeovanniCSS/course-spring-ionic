e733af7a502c5cbc1f3d21654d720be36ebdf13e
Geovanni Corsino <geovannicorsino@gmail.com>
2021-01-16 17:03:21 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/course/coursemc/resources/ClienteResource.java
+++ src/main/java/com/course/coursemc/resources/ClienteResource.java
@@ -3,12 +3,14 @@ package com.course.coursemc.resources;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,10 +34,12 @@ public class ClienteResource {
 	@Autowired
 	private ClienteService service;
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@GetMapping
-	public List<Cliente> list() {
-		List<Cliente> lista = new ArrayList<>();
-		return lista;
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());  
+		return ResponseEntity.ok().body(listDto);
 	}
 
 	@GetMapping(value = "/{id}")
@@ -44,6 +48,7 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@GetMapping(value = "/page")
 	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
@@ -62,6 +67,7 @@ public class ClienteResource {
 		return ResponseEntity.created(uri).build();
 	}
 
+	
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
 		Cliente obj = service.fromDTO(objDto);
@@ -70,6 +76,7 @@ public class ClienteResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
